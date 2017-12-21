9027b096372c3b76d304f4790083d57e23c2f9a7
Jason Rohrer <jasonrohrer@fastmail.fm>
2017-12-20 18:23:57 -0800

    Finished writing header.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # tinySockets
-Simple single-file C wrapper code for non-blocking sockets on Linux.
+Simple single-file C wrapper code for thread-free, non-blocking sockets on Linux.
+
+Wraps functionality in <sys/socket.h>
+
+No mallocs internally (except for any that underlying socket implementation does).  Size of static memory footprint confirgurable in header file based on max number of connections.
+
+Uses epoll for efficient handling of many non-blocking sockets.
---
+++ tinySockets.h
@@ -0,0 +1,97 @@
+// placed in the public domain by Jason Rohrer
+
+
+// change this value based on your application
+// internal memory is statically allocated with no mallocs
+#define MAX_LIVE_SOCKETS  1024
+
+
+
+//////////////////////
+// client functions //
+//////////////////////
+
+
+
+// returns:
+//   socket handle, or
+//   -1 on error
+int startConnecting( const char *inIPAddress, int inPort );
+
+
+
+// returns:
+//   1 if connected
+//   0 if still trying to connect
+//  -1 on error
+char isConnected( int inSocket );
+
+
+
+
+
+
+/////////////////////////////////
+// client and server functions //
+/////////////////////////////////
+
+
+
+// *outServerSocket is set to:
+//   true if the triggered socket is a server socket
+//   or
+//   false if the triggered socket is a connected socket
+// 
+// returns:
+//   socket handle for socket with data that can be received 
+//   or
+//   server socket handle for server socket that has a new connection waiting
+//   or
+//  -1 on error 
+int waitForSocketEvent( int inSocket, int inTimeoutInMilliseconds,
+                        char *outServerSocket );
+
+
+
+// returns:
+//   number of bytes sent (including, possibly, 0 if socket is blocked)
+//  -1 on error
+int send( int inSocket, unsigned char *inBytes, int inNumToSend );
+
+
+
+// returns:
+//   number of bytes received
+//  -1 on error
+int receive( int inSocket, unsigned char *inBytes, int inMaxNumToReceive );
+
+
+
+// closes either a client or a server socket
+void closeSocket( int inSocket );
+
+
+
+
+
+
+////////////////////// 
+// server functions //
+//////////////////////
+
+
+
+// returns:
+//   server socket handle
+//   or
+//  -1 on error
+int startServer( int inPort );
+
+
+
+// returns:
+//   socket handle for new connection
+//   or
+//   0 on timeout
+//  -1 on error
+int acceptConnection( int inServerSocket );
